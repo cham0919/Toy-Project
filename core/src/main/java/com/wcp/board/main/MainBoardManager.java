4b633612829160367bec6081d31a5f7177c0dea2
ckadl0919 <ckadl0118@gmail.com>
2021-04-22 23:21:40 +0900

    feat. 게시판 Paging 기능 추가
    게시판 Paging 기능 추가

--- core/src/main/java/com/wcp/board/main/MainBoardManager.java
+++ core/src/main/java/com/wcp/board/main/MainBoardManager.java
@@ -3,6 +3,10 @@ package com.wcp.board.main;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,6 +17,7 @@ public class MainBoardManager {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
     private MainBoardRepository mainBoardRepository;
 
     public void save(MainBoard mainBoard){
@@ -23,8 +28,15 @@ public class MainBoardManager {
         return mainBoardRepository.findById(id);
     }
 
-    public List<MainBoard> fetchAll(){
+    public List<MainBoard> findAll(){
         return mainBoardRepository.findAll();
+    }
+
+    public List<MainBoard> findByPage(int currentPage){
+        Page<MainBoard> mainBoards = mainBoardRepository
+                                        .findAll(PageRequest
+                                        .of(currentPage - 1, 10, Sort.by(Sort.Direction.ASC, "seq")));
+        return mainBoards.getContent();
     }
 
     public void update(MainBoard faqBoard){
@@ -34,5 +46,9 @@ public class MainBoardManager {
 
     public void remove(MainBoard mainBoard){
         mainBoardRepository.delete(mainBoard);
+    }
+
+    public Long count(){
+        return mainBoardRepository.count();
     }
 }
