63ddc19e98dc0ac6ce4a56100732d9c264cc8ca4
ckadl0919 <ckadl0118@gmail.com>
2021-04-24 18:48:53 +0900

    feat. Paging 기능 추가
    Paging 기능 추가

---
+++ core/src/main/java/com/wcp/board/page/Page.java
@@ -0,0 +1,13 @@
+package com.wcp.board.page;
+
+public interface Page {
+
+    int MAIN_PAGE_COUNT = 5;
+    int MAIN_POST_COUNT = 10;
+
+    int ADMIN_PAGE_COUNT = 5;
+    int ADMIN_POST_COUNT = 10;
+
+    int FAQ_PAGE_COUNT = 5;
+    int FAQ_POST_COUNT = 10;
+}
---
+++ core/src/main/java/com/wcp/board/page/PageService.java
@@ -0,0 +1,35 @@
+package com.wcp.board.page;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PageService {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    public void getPageList(PageInfo pageInfo){
+        calcEndPage(pageInfo);
+        log.info(pageInfo.toString());
+        calcStartPage(pageInfo);
+        log.info(pageInfo.toString());
+    }
+
+    private void calcEndPage(PageInfo pageInfo){
+        int endPage = (int) ((Math.ceil(pageInfo.currentPage() / (double)pageInfo.pageCount()) * pageInfo.pageCount()));
+        int tmpEndPage = (int)(Math.ceil(pageInfo.totalPostCount()/ (double) pageInfo.postCount()));
+
+        if( endPage > tmpEndPage ){ endPage =  tmpEndPage; }
+
+        pageInfo.endPage(endPage);
+    }
+
+    private void calcStartPage(PageInfo pageInfo){
+        int startPage = (pageInfo.endPage() - pageInfo.pageCount()) + 1;
+        startPage = startPage <= 0 ? 1 : startPage;
+        pageInfo.startPage(startPage);
+
+        if( startPage > pageInfo.endPage() ){ pageInfo.endPage(startPage); }
+    }
+}
