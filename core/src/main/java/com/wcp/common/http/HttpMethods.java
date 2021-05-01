0fd5e29942a3280bfb9e84520c5118508fc046a2
ckadl0919 <ckadl0118@gmail.com>
2021-05-01 23:11:25 +0900

    feat. HttpMethod Enum 추가
    HttpMethod Enum 추가

---
+++ core/src/main/java/com/wcp/common/http/HttpMethods.java
@@ -0,0 +1,16 @@
+package com.wcp.common.http;
+
+import org.apache.http.client.methods.HttpGet;
+
+public enum HttpMethods {
+
+    GET,
+    HEAD,
+    POST,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    TRACE;
+
+
+}
