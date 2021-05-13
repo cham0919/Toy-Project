ddbb51efcf309dd06cc3076581e3d1c4dc708906
ckadl0919 <ckadl0118@gmail.com>
2021-05-13 22:51:55 +0900

    feat. user 관리 Manager 추가
    user 관리 Manager 추가

---
+++ core/src/main/java/com/wcp/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.wcp.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findById(String id);
+
+}
