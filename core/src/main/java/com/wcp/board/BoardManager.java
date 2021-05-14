001c2b7562087ceda7dc1241bfd7c1d07a4d8d61
ckadl0919 <ckadl0118@gmail.com>
2021-05-14 23:30:44 +0900

    feat. Board Service 생성
    Board Service 생성

--- core/src/main/java/com/wcp/board/BoardManager.java
+++ core/src/main/java/com/wcp/board/BoardManager.java
@@ -1,5 +1,54 @@
 package com.wcp.board;
 
-public interface BoardManager {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BoardManager {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public void save(Board Board){
+        boardRepository.save(Board);
+    }
+
+    public List<Board> fetchByPage(int currentPage){
+        Page<Board> boardPage = boardRepository
+                .findAll(PageRequest
+                        .of(currentPage - 1, 10, Sort.by(Sort.Direction.ASC, "key")));
+        return boardPage.getContent();
+    }
+
+    public Optional<Board> fetchById(Long id){
+        return boardRepository.findById(id);
+    }
+
+    public List<Board> fetchAll(){
+        return boardRepository.findAll();
+    }
+
+    public void update(Board board){
+        Optional<Board> fetchBoard = fetchById(board.getKey());
+        fetchBoard = Optional.of(board);
+    }
+
+    public void delete(Board board){
+        boardRepository.delete(board);
+    }
+
+    public void deleteById(Long id){ boardRepository.deleteById(id); }
+
+    public Long count(){ return boardRepository.count(); }
 
 }
---
+++ core/src/main/java/com/wcp/board/BoardService.java
@@ -0,0 +1,78 @@
+package com.wcp.board;
+
+import com.wcp.page.PageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BoardService {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private BoardManager boardManager;
+
+    @Autowired
+    private PageService pageService;
+
+
+    /*
+    public void save(MainBoard mainBoard){
+        mainBoardManager.save(mainBoard);
+    }
+
+    public void delete(MainBoard mainBoard){
+        mainBoardManager.delete(mainBoard);
+    }
+
+    public void deleteById(String id){
+        if (StringUtils.isEmpty(id) || !StringUtils.isNumeric(id)) {
+            throw new IllegalArgumentException("id should not be empty or String. Please Check Id : "+ id);
+        }
+        mainBoardManager.deleteById(Long.valueOf(id));
+    }
+
+    public MainBoard fetchById(String id){
+        if(!StringUtils.isNumeric(id)){
+            throw new IllegalArgumentException();
+        }
+        return mainBoardManager.fetchById(Long.valueOf(id));
+    }
+
+    public List<MainBoard> fetchAll(){
+        return mainBoardManager.findAll();
+    }
+
+    public void update(MainBoard mainBoard) {
+        mainBoardManager.update(mainBoard);
+    }
+
+    public Long count(){
+        return mainBoardManager.count();
+    }
+
+    public List<MainBoard> findByPage(int currentPage){
+        return mainBoardManager.findByPage(currentPage);
+    }
+
+    public PageInfo getPageList(String currentPage){
+        if (StringUtils.isEmpty(currentPage) || !StringUtils.isNumeric(currentPage)) {
+            throw new IllegalArgumentException("currentPage should not be empty or String. Please Check currentPage : "+ currentPage);
+        }
+        return getPageList(Integer.valueOf(currentPage));
+    }
+
+    public PageInfo getPageList(int currentPage){
+        PageInfo pageInfo = PageInfo.of()
+                .currentPage(currentPage)
+                .pageCount(Page.MAIN_PAGE_COUNT)
+                .postCount(Page.MAIN_POST_COUNT)
+                .totalPostCount(this.count());
+        log.debug(pageInfo.toString());
+        pageService.getPageList(pageInfo);
+        return pageInfo;
+    }*/
+}
