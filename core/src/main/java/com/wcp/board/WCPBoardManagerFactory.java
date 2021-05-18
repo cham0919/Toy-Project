2dea59e2476a3319cc4d9c2bba7f49eb3aa119ba
ckadl0919 <ckadl0118@gmail.com>
2021-05-18 23:47:45 +0900

    feat. 게시판 factory 패턴 적용
    게시판 factory 패턴 적용

--- core/src/main/java/com/wcp/board/WCPBoardManagerFactory.java
+++ core/src/main/java/com/wcp/board/WCPBoardManagerFactory.java
@@ -1,12 +1,8 @@
 package com.wcp.board;
 
 
-import com.wcp.board.admin.AdminBoardManager;
-import com.wcp.board.faq.FAQBoardManager;
-import com.wcp.board.main.MainBoardManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("WCPBoardManagerFactory")
@@ -14,27 +10,17 @@ public class WCPBoardManagerFactory implements BoardManagerFactory {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    private MainBoardManager mainBoardManager;
-
-    @Autowired
-    private FAQBoardManager faqBoardManager;
-
-    @Autowired
-    private AdminBoardManager adminBoardManager;
-
-
     @Override
     public BoardManager create(String boardType) {
         if(boardType == null || boardType.length()==0){ throw new IllegalArgumentException("Given boardType is null or empty."); }
         log.debug("boardType : {}",boardType);
 
         if ( boardType.equalsIgnoreCase("main") ) {
-            return mainBoardManager;
+//            return mainBoardManager;
         } else if ( boardType.equalsIgnoreCase("faq") ) {
-            return faqBoardManager;
+//            return faqBoardManager;
         } else if( boardType.equalsIgnoreCase("admin") ) {
-            return adminBoardManager;
+//            return adminBoardManager;
         }
 
         throw new UnsupportedOperationException("Could not distinguish given boardType : "+boardType);
