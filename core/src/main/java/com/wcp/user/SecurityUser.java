e93b2ea647fe896864c1c182f1796bfc95b68a81
ckadl0919 <ckadl0118@gmail.com>
2021-05-27 18:48:07 +0900

    feat. CustomUserDetails 생성
    CustomUserDetails 생성

---
+++ core/src/main/java/com/wcp/user/SecurityUser.java
@@ -0,0 +1,64 @@
+package com.wcp.user;
+
+import com.wcp.security.Role;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class SecurityUser implements UserDetails {
+
+    private Long key;
+    private String id;
+    private String password;
+    private String email;
+    private String name;
+    private String nickname;
+    private String phone;
+    private LocalDateTime register_datetime;
+    private Role role;
+    private UserSataus status;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
