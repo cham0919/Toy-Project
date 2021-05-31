258fb8674deea40d925b37859849efb8f8aa00a1
ckadl0919 <ckadl0118@gmail.com>
2021-05-31 18:13:29 +0900

    feat. Judge 관리 class 생성
    Judge 관리 class 생성

---
+++ core/src/main/java/com/wcp/judge/Judge.java
@@ -0,0 +1,20 @@
+package com.wcp.judge;
+
+public class Judge {
+
+    public static final String URL = "https://judge0-ce.p.rapidapi.com";
+    public static final String TOKEN_KEY = "x-rapidapi-key";
+    public static final String TOKEN_VALUE = "b8648f0040msh885f2f3104a8cd9p17d505jsna839dc3d1d27";
+    public static final String HOST_KEY = "x-rapidapi-host";
+    public static final String HOST_VALUE = "judge0-ce.p.rapidapi.com";
+
+    public static final String WIDGET = URL +"/widget";
+    public static final String SUBSCRIPTIONDETAIL = URL + "/rapidapi";
+    public static final String SUBMISSION = URL + "/submissions";
+    public static final String BATCHSUBMISSION = URL + "/submissions" + "/batch";
+    public static final String STATUSES = URL + "/statuses";
+    public static final String PASTE = URL + "/paste";
+    public static final String LANGUAGES = URL + "/languages";
+    public static final String CONFIG = URL + "/config_info";
+
+}
