2f633dc25065dc304dcd7f299bd5bce7ad8aa8d1
ckadl0919 <ckadl0118@gmail.com>
2021-06-03 04:14:59 +0900

    chore. submitdto 변수 일부 수정
    submitdto 변수 일부 수정

--- core/src/main/java/com/wcp/coding/submit/SubmitHistoryDto.java
+++ core/src/main/java/com/wcp/coding/submit/SubmitHistoryDto.java
@@ -2,17 +2,21 @@ package com.wcp.coding.submit;
 
 import com.wcp.coding.test.CodingTestDto;
 import com.wcp.user.UserDto;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
 
+@Data
+@Accessors(chain = true)
 public class SubmitHistoryDto {
 
     private String key;
     private CodingTestDto codingTest;
     private UserDto user;
     private String status;
-    private String language;
-    private String code;
+    private String language_id;
+    private String source_code;
     private LocalDateTime submitAt;
     private String runTime;
 }
