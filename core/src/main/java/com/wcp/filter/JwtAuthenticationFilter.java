a56c43f025079612e3a880d863c33c23e8931925
ckadl0919 <ckadl0118@gmail.com>
2021-06-04 21:41:17 +0900

    feat. Jwt Filter 추가
    Jwt Filter 추가

---
+++ core/src/main/java/com/wcp/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.wcp.filter;
+
+import com.wcp.security.JwtAuthentication;
+import com.wcp.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 헤더에서 JWT 를 받아옵니다.
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        // 유효한 토큰인지 확인합니다.
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            setAuthentication(token);
+        }
+        chain.doFilter(request, response);
+
+    }
+
+
+    private void setAuthentication(String token) {
+        if (!token.isEmpty()) {
+            JwtAuthentication authentication = new JwtAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+    }
+
+}
