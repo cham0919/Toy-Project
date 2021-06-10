6138802759c6bdce4ca097d05e4d3fe84d9c2114
ckadl0919 <ckadl0118@gmail.com>
2021-06-10 22:04:37 +0900

    refac. Controller에 Entity 제거
    Controller에 Entity 제거

--- rest-api/src/main/java/com/wcp/user/UserController.java
+++ rest-api/src/main/java/com/wcp/user/UserController.java
@@ -2,13 +2,12 @@ package com.wcp.user;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.wcp.security.JwtTokenProvider;
+import com.wcp.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.security.Principal;
 import java.util.LinkedList;
 
@@ -32,7 +30,6 @@ public class UserController {
             .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
             .create();
     private final UserService userService;
-    private final JwtTokenProvider jwtTokenProvider;
 
 
     @RequestMapping(value = "/check", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
@@ -56,17 +53,17 @@ public class UserController {
     @ResponseBody
     public ResponseEntity<String> signUp(HttpServletRequest req,
                                             HttpServletResponse res,
-                                         @RequestBody User user)
+                                         @RequestBody UserDto userDto)
     {
         try{
-            user = userService.signUp(user);
-            return new ResponseEntity<String>(gson.toJson(user), HttpStatus.OK);
+            userDto = userService.signUp(userDto);
+            return new ResponseEntity<String>(gson.toJson(userDto), HttpStatus.OK);
         }catch (Throwable t){
             return new ResponseEntity<String>( HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    // 회원탈퇴
+   /* // 회원탈퇴
     @RequestMapping(value = "/deactivate", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
     @ResponseBody
     public ResponseEntity<String> deactivateUser(HttpServletRequest req,
@@ -79,28 +76,9 @@ public class UserController {
         }catch (Throwable t){
             return new ResponseEntity<String>( HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
+    }*/
 
-
-    // 로그인
-    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
-    @ResponseBody
-    public ResponseEntity<String> login(HttpServletRequest req,
-                                         HttpServletResponse res,
-                                        @RequestBody UserDto dto)
-    {
-        try{
-//            UserDetails user = userService.loadUserByUsername(dto.getId());
-            String token = jwtTokenProvider.createToken("test", new LinkedList(){{ add("ROLE_USER");}});
-            return new ResponseEntity<String>(token, HttpStatus.OK);
-        }catch (Throwable t){
-            log.error("error",t);
-            return new ResponseEntity<String>( HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-    // 로그아웃
-
-    // 아이디 찾기
+  /*  // 아이디 찾기
     @RequestMapping(value = "/findId", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
     @ResponseBody
     public ResponseEntity<String> findId(HttpServletRequest req,
@@ -144,5 +122,5 @@ public class UserController {
         }catch (Throwable t){
             return new ResponseEntity<String>( HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
+    }*/
 }
