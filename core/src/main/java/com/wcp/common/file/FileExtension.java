98a1bc1c26ea4f725ac57c96cf6b64252c8921ec
ckadl0919 <ckadl0118@gmail.com>
2021-06-11 21:43:41 +0900

    feat. FileExtention 추가
     FileExtention 추가

--- core/src/main/java/com/wcp/common/file/FileExtension.java
+++ core/src/main/java/com/wcp/common/file/FileExtension.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum FileExtension implements BaseEnum {
 
-    ZIP("zip");
+    ZIP("zip"),
+    PROPERTIES("properties");
 
     private String fileExtension;
 
