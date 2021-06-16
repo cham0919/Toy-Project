7420d21162acc309fa78f5ed91b32f4673ef2655
ckadl0919 <ckadl0118@gmail.com>
2021-06-16 20:00:20 +0900

    refac. url 변경
    url 변경

--- rest-api/src/main/java/com/wcp/coding/room/CodingRoomController.java
+++ rest-api/src/main/java/com/wcp/coding/room/CodingRoomController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/wcp/coding/board")
+@RequestMapping(value = "/wcp/coding/room")
 @RequiredArgsConstructor
 public class CodingRoomController {
 
