3910dd88b733025518e5819114344bcf83272572
ckadl0919 <ckadl0118@gmail.com>
2021-06-20 21:20:34 +0900

    chore. Mapper Build
    Mapper Build

--- core/src/main/generated/com/wcp/mapper/BoardCommantMapperImpl.java
+++ core/src/main/generated/com/wcp/mapper/BoardCommantMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-31T13:25:46+0900",
+    date = "2021-06-20T07:28:05+0900",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 1.8.0_231 (Oracle Corporation)"
 )
 @Component
