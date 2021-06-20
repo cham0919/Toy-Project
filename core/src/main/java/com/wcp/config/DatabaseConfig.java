0b5b179f1c7acf50e5e3037847ae9313a0b00d81
ckadl0919 <ckadl0118@gmail.com>
2021-06-20 21:18:13 +0900

    feat. QueryDSL 설정 추가
    QueryDSL 설정 추가

--- core/src/main/java/com/wcp/config/DatabaseConfig.java
+++ core/src/main/java/com/wcp/config/DatabaseConfig.java
@@ -2,17 +2,21 @@ package com.wcp.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@EnableJpaAuditing
+@Configuration
 public class QuerydslConfig {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+//    @PersistenceContext
+//    private EntityManager entityManager;
 
     @Bean
-    public JPAQueryFactory jpaQueryFactory() {
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
         return new JPAQueryFactory(entityManager);
     }
 }
