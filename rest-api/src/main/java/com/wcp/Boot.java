21ae83f8853556175b4854077953ac6966026008
ckadl0919 <ckadl0118@gmail.com>
2021-06-30 00:54:20 +0900

    chore. Boot run Class move
    Boot run Class move

--- rest-api/src/main/java/com/wcp/Boot.java
+++ rest-api/src/main/java/com/wcp/Boot.java
@@ -1,16 +1,16 @@
-//package com.wcp;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//
-//@EnableJpaRepositories
-//@EnableJpaAuditing
-//@SpringBootApplication
-//public class Boot {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(Boot.class, args);
-//    }
-//}
+package com.wcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+@EnableJpaAuditing
+@SpringBootApplication
+public class Boot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+}
