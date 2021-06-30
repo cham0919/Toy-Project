b3d8770999bda0dbff84a3c7ce75e900a3107916
ckadl0919 <ckadl0118@gmail.com>
2021-06-30 22:23:50 +0900

    feat. Advice Exception 추가
    Advice Exception 추가

--- rest-api/src/main/java/com/wcp/WCPAdvice.java
+++ rest-api/src/main/java/com/wcp/WCPAdvice.java
@@ -1,6 +1,7 @@
 package com.wcp;
 
 import com.wcp.error.ErrorResponse;
+import org.apache.tika.mime.MimeTypeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -8,22 +9,50 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.security.auth.login.LoginException;
+
 @RestControllerAdvice
 public class WCPAdvice {
 
     private final Logger log = LoggerFactory.getLogger(WCPAdvice.class);
 
     @ExceptionHandler(NumberFormatException.class)
-    public ResponseEntity<String> custom(NumberFormatException e) {
+    public ResponseEntity<ErrorResponse> numberFormatException(NumberFormatException e) {
         log.error("key should not be empty or String. Please Check key", e);
 
         final ErrorResponse response
                 = ErrorResponse
                 .create()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .message("key should not be empty or String. Please Check key");
+                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
 
-        return new ResponseEntity<String>(response.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(LoginException.class)
+    public ResponseEntity<ErrorResponse> loginException(LoginException e) {
+        log.error("Please check id and pw", e);
+
+        final ErrorResponse response
+                = ErrorResponse
+                .create()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+
+        return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<ErrorResponse> throwable(Throwable t) {
+        log.error(t.getMessage(), t);
+
+        final ErrorResponse response
+                = ErrorResponse
+                .create()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+
+        return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
